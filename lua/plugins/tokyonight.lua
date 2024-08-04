5f787eec922934062f5dbdcb640cec29a7e07e0f
kirisame-reimu <marisa15255620957@outlook.com>
2024-08-04 13:40:54 +0800

    transparent background

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,3 +1,7 @@
+-- manually installed to set transparent background
+-- 背景图片在终端中设置，nvim中只设置透明背景
+-- coppied from: https://lazyvim-github-io.vercel.app/zh-Hans/configuration/recipes#make-tokyonight-transparent
+
 return {
   "folke/tokyonight.nvim",
   opts = {
